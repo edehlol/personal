3e8710f8363cd092458f26b4ef05f10c8f8f2077
edy <edy@apyhub.com>
2022-11-20 12:30:42 +0700

    remove img

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import Image from "next/image";
-import img from "../public/img.png";
-
 export default function Home() {
   return (
     <div>
       <h1>My Personal Website</h1>
-      <Image src={img} alt="Sunset over a river" width={400} />
     </div>
   );
 }
